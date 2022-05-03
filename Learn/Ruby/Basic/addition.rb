f19b56adce4597dafb6d84eca8d116f09f3a1897
said-04 <said.t.j.04@gmail.com>
2022-05-03 12:09:56 -0500

    Script the addition.

---
+++ Learn/Ruby/Basic/addition.rb
@@ -0,0 +1,12 @@
+# This program is realice addition two number.
+
+# We define two variables.
+a = 10
+b = 90
+
+# The calculation is done.
+r_addition = a + b
+
+
+# Show the user.
+puts("The result of addition is", r_addition)
